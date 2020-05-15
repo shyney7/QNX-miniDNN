420c3d96b4f1f675a89ad68376ed4a010f0554f6
MarcelUbuntucross <ligimixi@gmail.com>
2020-05-15 03:28:32 -0700

    First sucessful random value training on QNX

---
+++ build/build-linux.sh
@@ -0,0 +1,2 @@
+g++ -O2 -I/home/marcel/Documents/libs/eigen -I/home/marcel/Documents/libs/MiniDNN/include main.cpp
+
---
+++ build/buildQNX.sh
@@ -0,0 +1,1 @@
+/home/marcel/qnx700/host/linux/x86_64/usr/bin/ntox86_64-c++ -O3 -I/home/marcel/Documents/libs/eigen -I/home/marcel/Documents/libs/MiniDNN/include ../main.cpp -o ./bin/dnnQNX
